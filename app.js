c14570fcd194283d6597b0bb18eb966359397519
Marisol <marisol.azofeifa@totemtech.net>
2023-03-15 21:27:18 -0600

    portafolio

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const button = document.getElementById("download-pdf");
 button.addEventListener("click", function() {
   const link = document.createElement("a");
-  link.href = "./img/CV.pdf";
+  link.href = "./img/marisolazofeifa.pdf";
   link.download = "marisolazofeifa.pdf";
   link.click();
 });
